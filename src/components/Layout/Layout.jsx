1e04d79cc8028d9e01bf8ad7d20f0f1622f946f1
Seifsaad <seifsaad2018@gmail.com>
2026-07-06 03:12:03 +0300

    5th commit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 
 export default function Layout() {
     return (
         <>
+            <ScrollRestoration />
             <Navbar />
             <section className="pt-15 pt-md-0 min-h-screen bg-gradient-to-br from-blue-50 via-white to-blue-100 overflow-hidden">
                 <Outlet />
